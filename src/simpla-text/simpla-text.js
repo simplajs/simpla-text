71147407c83d53efbf5721e4a6f160a8fa367f33
Bede <bede.overend@gmail.com>
2015-10-16 08:36:03 +1100

    Add editable behavior to simpla-text

--- src/simpla-text/simpla-text.js
+++ src/simpla-text/simpla-text.js
@@ -2,6 +2,12 @@ class SimplaText {
   beforeRegister() {
     this.is = 'simpla-text';
   }
+
+  get behaviors() {
+    return [
+      simpla.behaviors.editable
+    ];
+  }
 }
 
 Polymer(SimplaText);
