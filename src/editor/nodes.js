c1c1ca8eed1e60bdf5ef991acfb8dfd650880c0f
Bede Overend <bede.overend@gmail.com>
2017-03-20 12:52:26 +1100

    Use v0.19 Prosemirror, custom text node. Use forked git prosemirror-view to fix shadow dom bugs

--- src/editor/nodes.js
+++ src/editor/nodes.js
@@ -2,6 +2,7 @@ import { nodes } from 'prosemirror-schema-basic'
 
 const blockDoc = { content: 'block+' },
       inlineDoc = { content: 'inline<_>*' },
-      { paragraph, text, hard_break: hardBreak } = nodes;
+      { paragraph, hard_break: hardBreak } = nodes,
+      text = { group: 'inline', toDOM: (node) => node.text };
 
 export { paragraph, text, hardBreak, inlineDoc, blockDoc };
