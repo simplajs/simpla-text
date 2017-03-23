51ab6b94134c9366b35ce3fe21a1c3a0bf439602
Sean King <sean@seanking.org>
2017-03-23 12:14:16 +0100

    add toolbar UI

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -166,7 +166,7 @@ gulp.task('build:tests', ['build:tests:html', 'build:tests:js']);
 gulp.task('test', ['build', 'build:tests', 'test:local']);
 
 gulp.task('watch:src', () => gulp.watch(['src/**/*'], () => gulprun('build', 'refresh')));
-gulp.task('watch:tests', () => gulp.watch(['src/**/*', 'test/**/*'], () => gulprun('build:tests')))
+gulp.task('watch:tests', () => gulp.watch(['test/**/*'], () => gulprun('build:tests')))
 gulp.task('watch', ['watch:src', 'watch:tests']);
 
 gulp.task('default', ['build', 'build:tests', 'serve', 'watch']);
